e3caabb7fdfa542459fe6e1d9a0db8131cc4f1f9
kirachekaeva <m2408945@edu.misis.ru>
2025-05-05 04:16:24 +0900

    Blue_2.cs

--- Blue_2.cs
+++ Blue_2.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Lab_8
 {
-    internal class Blue_2: Blue
+    public class Blue_2: Blue
     {
         private string _output;
         private string _todel;
